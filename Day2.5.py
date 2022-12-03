562d2c969740fb6778767f65b02fd0b9a0ee9200
joonryul.lee 이준렬 <joonryul.lee@5minlab.com>
2022-12-03 20:00:58 +0900

    divided by day and added 2.5 solution

---
+++ Day2.5.py
@@ -0,0 +1,86 @@
+# day 2 https://adventofcode.com/2022/day/2
+
+# A - rock     X - lose
+# B - Paper    Y - draw
+# C - scissor  Z - win
+
+def result_score(opponent, mine):
+    if opponent == mine:
+        return 3
+    elif opponent == 'R':
+        if mine == 'P':
+            return 6
+        elif mine == 'S':
+            return 0
+    elif opponent == 'P':
+        if mine == 'S':
+            return 6
+        elif mine == 'R':
+            return 0
+    elif opponent == 'S':
+        if mine == 'R':
+            return 6
+        elif mine == 'P':
+            return 0
+    return 0
+
+
+def selection_score(mine):
+    if mine == 'R':
+        return 1
+    elif mine == 'P':
+        return 2
+    elif mine == 'S':
+        return 3
+    return 0
+
+
+print("Enter/Paste your content. Ctrl-D or Ctrl-Z ( windows ) to save it.")
+contents = []
+while True:
+    try:
+        line = input()
+    except EOFError:
+        break
+    contents.append(line)
+
+opponentSelection = []
+mySelection = []
+
+for s in contents:
+    if s[0] == 'A':
+        opponentSelection.append('R')
+        if s[2] == 'X':
+            mySelection.append('S')
+        elif s[2] == 'Y':
+            mySelection.append('R')
+        elif s[2] == 'Z':
+            mySelection.append('P')
+    elif s[0] == 'B':
+        opponentSelection.append('P')
+        if s[2] == 'X':
+            mySelection.append('R')
+        elif s[2] == 'Y':
+            mySelection.append('P')
+        elif s[2] == 'Z':
+            mySelection.append('S')
+    elif s[0] == 'C':
+        opponentSelection.append('S')
+        if s[2] == 'X':
+            mySelection.append('P')
+        elif s[2] == 'Y':
+            mySelection.append('S')
+        elif s[2] == 'Z':
+            mySelection.append('R')
+
+print(opponentSelection)
+print(mySelection)
+
+i = 0
+totalScore = 0
+for opponent in opponentSelection:
+    totalScore += result_score(opponent, mySelection[i])
+    totalScore += selection_score(mySelection[i])
+    i += 1
+
+print(totalScore)
--- Day2.py
+++ Day2.py
@@ -73,38 +73,3 @@ for opponent in opponentSelection:
     i += 1
 
 print(totalScore)
-
-# day 1 https://adventofcode.com/2022/day/1
-
-# print("Enter/Paste your content. Ctrl-D or Ctrl-Z ( windows ) to save it.")
-# contents = []
-# while True:
-#     try:
-#         line = input()
-#     except EOFError:
-#         break
-#     contents.append(line)
-#
-# currentElfCalorie = 0
-#
-# highestElfCalorie = 0
-# highestElfCalorie2 = 0
-# highestElfCalorie3 = 0
-#
-# for s in contents:
-#     if s == '':
-#         if currentElfCalorie > highestElfCalorie:
-#             highestElfCalorie3 = highestElfCalorie2
-#             highestElfCalorie2 = highestElfCalorie
-#             highestElfCalorie = currentElfCalorie
-#         elif currentElfCalorie > highestElfCalorie2:
-#             highestElfCalorie3 = highestElfCalorie2
-#             highestElfCalorie2 = currentElfCalorie
-#         elif currentElfCalorie > highestElfCalorie3:
-#             highestElfCalorie3 = currentElfCalorie
-#         currentElfCalorie = 0
-#     else:
-#         currentElfCalorie += int(s)
-#
-# totalTop3 = highestElfCalorie + highestElfCalorie2 + highestElfCalorie3
-# print(totalTop3)
